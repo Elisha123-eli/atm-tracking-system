49e25d4bf70e80bc7320714e05d0fe175f01e75d
Elisha123-eli <albertelishakabesha02@gmail.com>
2026-03-23 21:16:31 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,7 @@ export default function Home() {
   return (
     <div style={{ padding: 20 }}>
       <h1>ATM Tracking System</h1>
-
-      <p>Welcome. System is starting...</p>
-
-      <br />
-
-      <button onClick={() => window.location.href='/tracking'}>
-        Tracking Page
-      </button>
-
-      <br /><br />
-
-      <button onClick={() => window.location.href='/summary'}>
-        Summary Page
-      </button>
-
-      <br /><br />
-
-      <button onClick={() => window.location.href='/admin'}>
-        Admin Panel
-      </button>
+      <p>System is running...</p>
     </div>
   );
 }
